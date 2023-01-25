71d0524850534bfcc5329c97e5ce62d1d18dbbcb
Nathan Lubawski <nathanlubawski1@gmail.com>
2023-01-25 15:15:49 -0300

    feat: create UserContext and update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,18 @@
-function App(){
-  return(
-    <h1>OK</h1>
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { useState } from "react";
+import UserContext from './context/UserContext';
+
+
+function App() {
+  const [token, setToken] = useState({})
+  const [userId, setUserId] = useState({})
+  return (
+    <UserContext.Provider value={{ token, setToken, userId, setUserId }} >
+      <BrowserRouter>
+        <Routes>
+        </Routes>
+      </BrowserRouter>
+    </UserContext.Provider>
   )
 }
 
