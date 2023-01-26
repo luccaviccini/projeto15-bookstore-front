20d40661aaee15374b1fe19d789327fc1ffc951c
me-luc <lukasalmeida372@gmail.com>
2023-01-26 17:28:57 -0300

    build: add font awe object for icons

--- src/components/payment-method/PaymentMethodSection.js
+++ src/components/payment-method/PaymentMethodSection.js
@@ -1,6 +1,7 @@
 import PaymentMethodButton from "./PaymentMethodButton";
 import { BsFillCreditCard2BackFill as CardIcon } from "react-icons/bs";
 import { BiBarcode as BarCodeIcon } from "react-icons/bi";
+import { FontawesomeObject } from "@fortawesome/fontawesome-svg-core";
 
 function PaymentMethodSection() {
 	return (
@@ -8,6 +9,7 @@ function PaymentMethodSection() {
 			<PaymentMethodButton Icon={CardIcon} text="Credit/Debit Card" />
 			<PaymentMethodButton Icon={CardIcon} text="Credit/Debit Card" />
 			<PaymentMethodButton Icon={BarCodeIcon} text="Bank Slip" />
+			<FontAwesomeIcon icon="fa-brands fa-pix" />
 		</>
 	);
 }
