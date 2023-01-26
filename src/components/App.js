2200efbc05d027b306709f6e258bbdb8d667c843
me-luc <lukasalmeida372@gmail.com>
2023-01-26 16:39:03 -0300

    feat: add checkout page

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,28 @@
 import { BagPage } from "../pages/index";
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useState } from "react";
-import UserContext from './context/UserContext';
+import UserContext from "./context/UserContext";
 import SignUp from "./SignUp";
 import SignIn from "./SignIn";
 
 function App() {
-  const [token, setToken] = useState({})
-  const [userId, setUserId] = useState({})
-  const [session, setSession] = useState({})
-  
-  return (
-    <UserContext.Provider value={{ token, setToken, userId, setUserId, session, setSession }} >
-      <BrowserRouter>
-        <Routes>
-          <Route path="/sign-up" element={<SignUp />} />
-          <Route path="/sign-in" element={<SignIn />} />
-          <Route path="/my-bag" element={<BagPage />} />
-        </Routes>
-      </BrowserRouter>
-    </UserContext.Provider>
-  )
+	const [token, setToken] = useState({});
+	const [userId, setUserId] = useState({});
+	const [session, setSession] = useState({});
+
+	return (
+		<UserContext.Provider
+			value={{ token, setToken, userId, setUserId, session, setSession }}>
+			<BrowserRouter>
+				<Routes>
+					<Route path="/sign-up" element={<SignUp />} />
+					<Route path="/sign-in" element={<SignIn />} />
+					<Route path="/my-bag" element={<BagPage />} />
+					<Route path="/checkout" element={<CheckoutPage />} />
+				</Routes>
+			</BrowserRouter>
+		</UserContext.Provider>
+	);
 }
 
 export default App;
---
+++ src/pages/CheckoutPage.js
@@ -0,0 +1,5 @@
+function CheckoutPage() {
+	return <h1>PAGE</h1>;
+}
+
+export default CheckoutPage;
